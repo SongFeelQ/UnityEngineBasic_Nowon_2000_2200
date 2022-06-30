0c42386521b9cedf3673d91f5335f79aa7303f0b
SongFeelQ <haseueh@gmail.com>
2022-06-30 22:01:21 +0900

    C# - Example4_HorseRacing

--- Enum_SwitchCase/Program.cs
+++ Enum_SwitchCase/Program.cs
@@ -1,15 +1,31 @@
 ﻿using System;
 // enum (enumerated type) 열거형
 // enum의 기본요소는 모두 int형
+// enum의 기본형은 한번에 하나의 요소만 표현가능하다.
 enum PlayerState
 {
-    Idle,
-    Attack,
-    Jump,
-    Walk,
-    Run,
-    Dash,
-    Home,
+    Idle,   // ... 00000000
+    Attack, // ... 00000001
+    Jump,   // ... 00000010
+    Walk,   // ... 00000011
+    Run,    // ... 00000100
+    Dash,   // ... 00000101
+    Home,   // ... 00000110
+}
+
+// Flags Attribute
+// ToString()에서 명시되지 않은 enum 요소에 대해서 다른 요소들로 구성되는 문자열로 변환해줄 수 있는 요소
+// Attribute
+[Flags]
+enum PlayerStateFlags
+{
+    Idle = 0 << 0,      // ... 00000000
+    Attack = 1 << 0,    // ... 00000001
+    Jump = 1 << 1,      // ... 00000010
+    Walk = 1 << 2,      // ... 00000100
+    Run = 1 << 3,       // ... 00001000
+    Dash = 1 << 4,      // ... 00010000
+    Home = 1 << 5,      // ... 00100000
 }
 
 enum studentName
@@ -22,17 +38,22 @@ namespace Enum_SwitchCase
 {
     internal class Program
     {
-        static bool doAttack;
+        static bool doAttack = true;
         static bool doJump;
         static bool doWalk;
         static bool doRun;
         static bool doDash;
         static bool doHome;
 
-        static PlayerState initState = PlayerState.Attack;
+        static PlayerState initState = PlayerState.Attack | PlayerState.Jump;
+        static PlayerStateFlags flags = PlayerStateFlags.Attack | PlayerStateFlags.Jump;
+
+
 
         static void Main(string[] args)
         {
+            Console.WriteLine(flags);
+
             Warrior warrior = new Warrior();
             warrior.name = "지존전사";
 
@@ -77,7 +98,69 @@ namespace Enum_SwitchCase
                     break; // break : 현재 구문을 빠져나오는 분기문
             }
 
+            // 분기문
+            // continue : 코드 흐름을 여기서 끝내고 실행중인 구문의 조건으로 돌아와서 계속 진행
+            // break : 코드 흐름을 여기서 끝내고 실행중인 구문을 빠져나옴
+            // return : 코드 흐름을 여기서 끝내고 함수 제어권 및 값 반환
+            for (int i = 0; i < 5; i++)
+            {
+                int a = 5;
+                continue;
+                a = 3;
+            }
 
+            string studentName = "";
+            switch (studentName)
+            {
+                case "철수":
+                    break;
+                case "영희":
+                    break;
+                default:
+                    break;
+            }
+
+            // 동작 명령
+            while (true)
+            {
+                Console.WriteLine("전사에게 명령을 내려주세요");
+                string order = Console.ReadLine();
+                if (order == "exit")
+                    return;
+
+                if (Enum.TryParse<PlayerState>(order, out PlayerState orderState))
+                {
+                    switch (orderState)
+                    {
+                        case PlayerState.Idle:
+                            break;
+                        case PlayerState.Attack:
+                            warrior.Attack();
+                            break;
+                        case PlayerState.Jump:
+                            warrior.Jump();
+                            break;
+                        case PlayerState.Walk:
+                            warrior.Walk();
+                            break;
+                        case PlayerState.Run:
+                            warrior.Run();
+                            break;
+                        case PlayerState.Dash:
+                            warrior.Dash();
+                            break;
+                        case PlayerState.Home:
+                            warrior.Home();
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("동작 입력이 올바르지 않습니다.");
+                }
+            }
 
         }
     }
---
+++ Example4_HorseRacing/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Threading;
+
+// - 진행방식 -
+//
+// 프로그램 시작시
+// 말 다섯마리를 만들고
+// 각 다섯마리는 초당 10~20 (정수형) 범위 거리를 랜덤하게 움직임
+// 각각의 말이 거리 200 에 도달하면 말의 이름과 등수를 출력해줌
+   
+// 말은
+// 이름, 달린거리 를 멤버변수로
+// 달리기 를 멤버 함수로 가짐.
+// 달리기 멤버함수는 입력받은 거리를 달린거리에 더해주어서 달린거리를 누적시키는 역할을 함.
+   
+// 매초 달릴 때 마다 각 말들이 얼마나 거리를 이동했는지 콘솔창에 출력해줌.
+// 경주가 끝나면 1,2,3,4,5 등 말의 이름을 등수 순서대로 콘솔창에 출력해줌.
+   
+// - Hint -
+   
+//- System.Threading namespace 에 있는 Thread.Sleep(1000); 를 사용하면 현재 프로그램을 1초 지연시킬수 있음
+// While 반복문에서 Thread.Sleep(1000); 을 추가하면 1초에 한번씩 반복문을 실행함.
+//- 말들이 동시에 들어오는 경우에 대해서는 그냥 말 이름 순으로 등수 책정
+
+
+
+namespace Example4_HorseRacing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int StartPoint = 0;
+            int minSpeed = 10;
+            int maxSpeed = 20;
+            Random random = new Random();
+            const int FinishPoint = 200;
+            bool gameFinished = false;
+
+            // 말 다섯마리를 만든다.
+            Horse[] horse = new Horse[5];
+            // 각각의 말 이름들도 해준다.
+            for (int i = 0; i < 5; i++)
+            {   
+                horse[i] = new Horse();
+                Console.Write($"{i+1}번 말 이름을 정해주세요 : ");
+                horse[i].name = Console.ReadLine();
+                horse[i].range = StartPoint;
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{i+1}번 말 이름: {horse[i].name}");
+            }
+
+            // 경주
+            int count = 0;
+            while (gameFinished == false)
+            {
+                count++;
+                Console.WriteLine($"===== 시작 {count} 초 경과 =====");
+                for (int i = 0; i < 5; i++)
+                {
+                    if (horse[i].dontMove == false & horse[i].range < FinishPoint)
+                    {
+                        int tmpMoveDistance = random.Next(minSpeed, maxSpeed + 1);
+                        horse[i].Run(tmpMoveDistance);
+                        Console.Write($"{horse[i].name}가 달린 거리 :{tmpMoveDistance} / ");
+                    }
+                    Console.WriteLine($"{horse[i].name}의 현재 거리 :{horse[i].range}");
+
+                    if (horse[i].range >= FinishPoint)
+                    {
+                        horse[i].dontMove = true;
+                    }
+                }
+
+                Thread.Sleep(1000);
+            }
+        }
+    }
+
+    public class Horse
+    {
+        internal string name;
+        internal int range;
+        internal bool dontMove;
+
+        public void Run(int moveDistance)
+        {
+            range += moveDistance;
+        }
+
+    }
+}
